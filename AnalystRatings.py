b31f8bf41548688d1940565e5950876d0c2bc906
Rohit Arora <rohitarora21@gmail.com>
2018-08-23 05:43:50 +0000

    New update RA

--- AnalystRatings.py
+++ AnalystRatings.py
@@ -1,10 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.marketwatch.com/investing/stock/hpe/analystestimates"
+url = "https://www.marketwatch.com/investing/stock/sq/analystestimates"
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
-print(soup.title)
-stock_value = soup.find_all('a',attrs={"class":"list__item"})
-#stock_value = soup.find('a', {"class":"list_item"})
-print(stock_value)
+
+
+def marketWatchAnalystRating(soup):
+    data = []
+    print(soup.title)
+    table = soup.find('table', attrs={'class': 'ratings'})
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        data.append(cols[0].text.strip() + " " + cols[1].text.strip())
+
+    print("##",data)
+
+def yahooStatisticsData(soup):
+
+
+
+
+#marketWatchAnalystRating(soup)
+
